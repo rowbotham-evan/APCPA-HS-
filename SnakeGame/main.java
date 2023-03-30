b444d1591fca27447b8f140430da58982a1b308f
rowbotham-evan <123652433+rowbotham-evan@users.noreply.github.com>
2023-03-30 10:07:08 -0400

    Create main.java

---
+++ SnakeGame/main.java
@@ -0,0 +1,12 @@
+// *****************************************
+// Evan Rowbotham, Nick Cremona, David Pritchard, Yuvi Gohil, Parth Pandya
+// *****************************************
+
+// MAIN CLASS
+public class Main 
+{
+ 	public static void main(String[] args)
+  {
+  	IntroScreen is = new IntroScreen();
+	}
+}
